976566e07925bad0ad9c77ad2794f36ad77132da
Lumit <to.be.impressive@gmail.com>
2016-04-28 16:40:34 +0800

    Reduce annoying alert and add multi-language support for alerts. Now change language will also change HTML's lang attr.Replace all quotes with single quotes

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 $(function() {
   'use strict';
   var isSuccess = false;
-  var lang;
+  var language = 'en';
 
 
   /** ------------------------
@@ -9,63 +9,70 @@ $(function() {
    * multi-language
    *
    * ------------------------ */
-  var langDict = {
-    english: {
-      "lang-1": "<div>This may be the most dangerous app. You have to keep writing, or everything will be erased if you stop beyond the expiring time.</div><div>Now set the duration time you want to focus and the exciting expiring time.</div>",
-      "lang-2": "Lasts for(minutes):",
-      "lang-3": "Expires in(seconds):",
-      "lang-4": "Begin",
-      "lang-5": "Time's up :( <br> Be more focus next time.",
-      "lang-6": "Again",
-      "lang-7": "Reset time",
-      "lang-8": "Good job!<br />Would you like to go back and copy what you have written?",
-      "lang-9": "Go back to copy",
-      "lang-10": "Give up"
+  const langDict = {
+    en: {
+      'lang-1': '<div>This may be the most dangerous app. You have to keep writing, or everything will be erased if you stop beyond the expiring time.</div><div>Now set the duration time you want to focus and the exciting expiring time.</div>',
+      'lang-2': 'Lasts for(minutes):',
+      'lang-3': 'Expires in(seconds):',
+      'lang-4': 'Begin',
+      'lang-5': 'Time\'s up :( <br> Be more focus next time.',
+      'lang-6': 'Again',
+      'lang-7': 'Reset time',
+      'lang-8': 'Good job!<br />Would you like to go back and copy what you have written?',
+      'lang-9': 'Go back to copy',
+      'lang-10': 'Give up',
+      'prompt': 'Please finish your time setting'
     },
-    chinese: {
-      "lang-1": "<div>这也许是世界上最危险的写作应用：在你设定的时间段内，如果你停止输入文字超过设定的间隔时间，之前写下的所有内容都会消失。</div><div>现在请设定持续时间段和间隔时间。</div>",
-      "lang-2": "持续时间（分）：",
-      "lang-3": "间隔时间（秒）：",
-      "lang-4": "开始",
-      "lang-5": "时间到 _(:3 」∠)_ <br> 下次要更加专心哦~",
-      "lang-6": "再来一次",
-      "lang-7": "重设时间",
-      "lang-8": "赞√<br />你想回去复制刚刚写的内容吗？",
-      "lang-9": "回去复制",
-      "lang-10": "我要重写"
+    zh: {
+      'lang-1': '<div>这也许是世界上最危险的写作应用：在你设定的时间段内，如果你停止输入文字超过设定的间隔时间，之前写下的所有内容都会消失。</div><div>现在请设定持续时间段和间隔时间。</div>',
+      'lang-2': '持续时间（分）：',
+      'lang-3': '间隔时间（秒）：',
+      'lang-4': '开始',
+      'lang-5': '时间到 _(:3 」∠)_ <br> 下次要更加专心哦~',
+      'lang-6': '再来一次',
+      'lang-7': '重设时间',
+      'lang-8': '赞√<br />你想回去复制刚刚写的内容吗？',
+      'lang-9': '回去复制',
+      'lang-10': '我要重写',
+      'prompt': '请设定好时间'
     },
-    japanese: {
-      "lang-1": "<div>これは世界中の一番危ないアプリかもしれません：あなたのセッティングした間には、書き続けなければなりません。もしそうでなく、全ての入力したデータは削除されます。</div><div>今、持続時間と制限時間をセッティングしましょう！</div>",
-      "lang-2": "持続時間（分）：",
-      "lang-3": "制限時間（秒）：",
-      "lang-4": "スタート",
-      "lang-5": "時間切れ _(:3 」∠)_ <br> 今度は頑張ってくださいね～",
-      "lang-6": "もう一度",
-      "lang-7": "リセット",
-      "lang-8": "すっごい√<br />戻ってデータを保存していいですか",
-      "lang-9": "戻る",
-      "lang-10": "書き直したい"
+    jp: {
+      'lang-1': '<div>これは世界中の一番危ないアプリかもしれません：あなたのセッティングした間には、書き続けなければなりません。もしそうでなく、全ての入力したデータは削除されます。</div><div>今、持続時間と制限時間をセッティングしましょう！</div>',
+      'lang-2': '持続時間（分）：',
+      'lang-3': '制限時間（秒）：',
+      'lang-4': 'スタート',
+      'lang-5': '時間切れ _(:3 」∠)_ <br> 今度は頑張ってくださいね～',
+      'lang-6': 'もう一度',
+      'lang-7': 'リセット',
+      'lang-8': 'すっごい√<br />戻ってデータを保存していいですか',
+      'lang-9': '戻る',
+      'lang-10': '書き直したい',
+      'prompt': '時間を設定してください'
     }
   };
 
-  $("[name=" + "english" + "]").addClass("hidden");
+
+  $('[name=' + language + ']').addClass('hidden');
 
   function changeLanguage(lang) {
     for (var key in langDict) {
-      $("[name=" + key + "]").removeClass("hidden");
+      $('[name=' + key + ']').removeClass('hidden');
     }
-    $("[name=" + lang + "]").addClass("hidden");
+    $('[name=' + lang + ']').addClass('hidden');
+    $('html').attr('lang', lang);
     var chosenLang = langDict[lang];
     for (var langKey in chosenLang) {
       var langValue = chosenLang[langKey];
-      $("." + langKey).html(langValue);
+      $('.' + langKey).html(langValue);
     }
   }
 
-  $(".langSet").bind("click", function() {
-    lang = $(this).attr("name");
-    changeLanguage(lang);
-  })
+  $('.langSet').bind('click', function() {
+    language = $(this).attr('name');
+    changeLanguage(language);
+  });
+
+
 
 
 
@@ -76,26 +83,26 @@ $(function() {
    *
    * ------------------------ */
   var simplemde = new SimpleMDE({
-    element: $("#MyID")[0]
+    element: $('#MyID')[0]
   });
   simplemde.value('');
 
   // let the height of typingarea adaptive
-  // I didn't put them in the "begin" function because input GUI of mobiles would influence the height of window.innerHeight
-  var $header = $("header");
+  // I didn't put them in the 'begin' function because input GUI of mobiles would influence the height of window.innerHeight
+  var $header = $('header');
   var $headerHeight = $header.height();
 
-  var $footer = $("footer");
+  var $footer = $('footer');
   var $footerHeight = $footer.height();
 
-  var $mainWrapper = $(".main-wrapper");
+  var $mainWrapper = $('.main-wrapper');
 
-  var $codeMirror = $(".CodeMirror");
+  var $codeMirror = $('.CodeMirror');
 
   var pre = window.innerHeight - $headerHeight - $footerHeight;
   $mainWrapper.height(pre);
   $codeMirror.height(pre - 120);
-  $codeMirror.css("background-color", "lightblue");
+  $codeMirror.css('background-color', 'lightblue');
 
 
 
@@ -107,54 +114,50 @@ $(function() {
   var remainingSet, expireSet, remainingOrigin, expireOrigin;
 
   // check whether the input for time is number
-  $("form.intro :input").bind("blur", function() {
-    if ($(this).is("#remainingTime")) {
+  $('form.intro :input').bind('blur', function() {
+    if ($(this).is('#remainingTime')) {
       if (this.value > 0) {
         remainingSet = this.value * 60 * 1000;
         // save the initial set and prepare for users' choice to write again
         remainingOrigin = remainingSet;
-      } else {
-        alert("Please input positive numbers");
       }
     }
-    if ($(this).is("#expireTime")) {
+    if ($(this).is('#expireTime')) {
       if (this.value > 0) {
         expireSet = this.value * 1000;
         expireOrigin = expireSet;
-      } else {
-        alert("Please input positive numbers");
       }
     }
   })
 
-  $("button.submit").bind("click", function() {
-    if ((typeof remainingSet) == "number" && (typeof expireSet) == "number") {
-      $("div.fullpage-intro").css("display", "none");
+  $('button.submit').bind('click', function() {
+    if ((typeof remainingSet) == 'number' && (typeof expireSet) == 'number') {
+      $('div.fullpage-intro').css('display', 'none');
       begin();
     } else {
-      alert("Please input two positive numbers");
+      alert(langDict[language]['prompt']);
     }
   })
 
   var begin = function() {
     // show the remaining time and expiring time
-    var $remainingShow = $("#remaining")
-    var $expireShow = $("#expire")
+    var $remainingShow = $('#remaining')
+    var $expireShow = $('#expire')
 
     if (remainingSet <= 60000) {
-      $remainingShow.text((remainingSet / 1000).toFixed(1) + "\"");
+      $remainingShow.text((remainingSet / 1000).toFixed(1) + '\'');
     } else {
-      $remainingShow.text((remainingSet / 60000).toFixed(2) + "\'");
+      $remainingShow.text((remainingSet / 60000).toFixed(2) + '\'');
     }
 
-    $expireShow.text((expireSet / 1000).toFixed(1) + "\"");
+    $expireShow.text((expireSet / 1000).toFixed(1) + '\'');
 
 
     function setExpire(value) {
       var value = value - 100;
-      $expireShow.text((value / 1000).toFixed(1) + "\"");
+      $expireShow.text((value / 1000).toFixed(1) + '\'');
       if (value <= 0) {
-        // if both the time equal 0 at the same time, don't show the ".fail" overlay
+        // if both the time equal 0 at the same time, don't show the '.fail' overlay
         if (remainingSet + 100 == expireSet) {
           return false;
         } else {
@@ -162,7 +165,7 @@ $(function() {
           clearInterval(timer);
           $codeMirror.stop(true, true);
           if (remainingSet != expireSet) {
-            $("div.fullpage-overlay.fail").removeClass("hidden");
+            $('div.fullpage-overlay.fail').removeClass('hidden');
           }
         }
       }
@@ -170,17 +173,17 @@ $(function() {
 
     function setRemaining(value) {
       if (value <= 60000) {
-        $remainingShow.text((remainingSet / 1000).toFixed(1) + "\"");
+        $remainingShow.text((remainingSet / 1000).toFixed(1) + '\'');
       } else {
-        $remainingShow.text((remainingSet / 60000).toFixed(2) + "\'");
+        $remainingShow.text((remainingSet / 60000).toFixed(2) + '\'');
       }
       if (value <= 0) {
-        // alert ("remaining"+value);
+        // alert ('remaining'+value);
         clearInterval(timer);
         isSuccess = true;
         $codeMirror.stop();
-        $codeMirror.css("opacity", "1");
-        $("div.fullpage-overlay.success").removeClass("hidden");
+        $codeMirror.css('opacity', '1');
+        $('div.fullpage-overlay.success').removeClass('hidden');
       }
     }
 
@@ -196,11 +199,11 @@ $(function() {
       opacity: '0'
     }, expireOrigin);
     // remember to clear the animation queue first
-    simplemde.codemirror.on("change", function() {
+    simplemde.codemirror.on('change', function() {
       if (isSuccess === false) {
         expireSet = expireOrigin;
         $codeMirror.stop(true, true);
-        $codeMirror.css("opacity", "1");
+        $codeMirror.css('opacity', '1');
         $codeMirror.animate({
           opacity: '0'
         }, expireOrigin);
@@ -216,13 +219,13 @@ $(function() {
    * overlay (success or fail)
    *
    * ------------------------ */
-  $(".go-copy").bind("click", function() {
-    $("div.fullpage-overlay").addClass("hidden");
+  $('.go-copy').bind('click', function() {
+    $('div.fullpage-overlay').addClass('hidden');
   });
 
-  $(".write-again").bind("click", function() {
-    $("div.fullpage-overlay").addClass("hidden");
-    $codeMirror.css("opacity", "1");
+  $('.write-again').bind('click', function() {
+    $('div.fullpage-overlay').addClass('hidden');
+    $codeMirror.css('opacity', '1');
     simplemde.value('');
     remainingSet = remainingOrigin;
     expireSet = expireOrigin;
